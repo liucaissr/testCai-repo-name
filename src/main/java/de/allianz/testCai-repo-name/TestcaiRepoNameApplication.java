7e6ad74f5e445a9dea0b25feb864ee59ff3c2caa
CaiLiu <liucaissr@gmail.com>
2022-10-27 10:13:10 +0200

    Create maven project

---
+++ src/main/java/de/allianz/testCai-repo-name/TestcaiRepoNameApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.testCai-repo-name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcaiRepoNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcaiRepoNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/testCai-repo-name/TestcaiRepoNameApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.testCai-repo-name;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcaiRepoNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
